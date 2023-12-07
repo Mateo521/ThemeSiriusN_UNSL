78da22e6d42843e96a9c407a193f4430da129f07
Mateo521 <mateovillalva42@gmail.com>
2023-12-07 09:45:58 -0300

    sidebar

--- sidebar.php
+++ sidebar.php
@@ -10,48 +10,50 @@ if (is_single())
         */ if (is_active_sidebar('sidebar-default')) { ?><div class="sidebar-default"><?php dynamic_sidebar('sidebar-default'); ?></div><?php } ?>
 
 <?php
-// Recupera las publicaciones más leídas
+
 $popular_posts = pvc_get_most_viewed_posts();
 
-// Verifica si hay publicaciones para mostrar
 if ($popular_posts) :
 ?>
     <div class="sidebar-widget">
         <h3 style="margin:0; padding:0 0 10px 0;">Noticias más leídas</h3>
         <ul>
             <?php
-            $post_count = 0; // Inicializa el contador de publicaciones
+            $post_count = 0;
 
             foreach ($popular_posts as $post) : setup_postdata($post);
                 if ($post_count >= 6) {
-                    // Si ya hemos alcanzado el límite de 6 noticias, salir del bucle
+
                     break;
                 }
             ?>
-                <li style="list-style-type: none; display:flex; justify-content:space-between;align-items:start; gap:10px; padding:5px 0 ;border-bottom: 1px dashed #999;">
-                    <p>
-                        <a href="<?php the_permalink(); ?>">
-                            <?php the_title(); ?>
-                        </a>
-                    </p>
+                <li style="list-style-type: none; display:flex; justify-content:space-between;align-items:center; gap:10px; padding:5px 0 ;border-bottom: 1px dashed #999;">
+                    <div style="display: flex;gap:15px;">
+                        <span style="color: #121263;
+    font-weight: 700;"><?php echo ($post_count + 1); ?></span>
+                        <p>
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_title(); ?>
+                            </a>
+                        </p>
+                    </div>
+
                     <?php
                     $thumbnail = get_the_post_thumbnail(get_the_ID(), 'thumbnail');
 
-                    // Muestra la miniatura si está disponible
                     if ($thumbnail) :
                     ?>
                         <img style="width:75px;height:75px;" src="<?php echo esc_url(get_the_post_thumbnail_url(get_the_ID(), 'thumbnail')); ?>" alt="">
                     <?php endif; ?>
 
                 </li>
-                <?php
-                $post_count++; // Incrementa el contador de publicaciones
+            <?php
+                $post_count++;
             endforeach;
             ?>
         </ul>
     </div>
-    <?php
-    // Restablece la información de la publicación después de usar la consulta personalizada
+<?php
     wp_reset_postdata();
 endif;
 ?>
