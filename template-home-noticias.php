2fdb51688e6ead04ec32b385f21bddac372ec53d
Mateo521 <mateovillalva42@gmail.com>
2023-12-21 08:52:45 -0300

    fixs

--- template-home-noticias.php
+++ template-home-noticias.php
@@ -1,36 +1,26 @@
 <?php
 get_header();
-
-
 //get_template_part('parts/home', 'main');
 // Sección Slider
 get_template_part('parts/home', 'slider');
 // Sección Info slider
 get_template_part('parts/home', 'info');
-
 // Sección Entrevistas
 get_template_part('parts/home', 'interview');
-
 // Sección Etiquetas
 get_template_part('parts/home', 'etiquetas');
-
 // Sección Institucional
-get_template_part('parts/home', 'institutional');
-
+get_template_part('parts/home', 'institutional')
 // Sección Destacada
 get_template_part('parts/home', 'outstanding');
-
 // Sección Medios
 get_template_part('parts/home', 'media');
-
 // Sección audiovisual
 get_template_part('parts/home', 'audiovisual');
-
 /* Fila Ciencia y Sociedad
 ------------------------------------------------------------------------------------------------------*/
 printf('<div class="container sys">
 			<div class="row">');
-
 get_template_part('parts/home', 'science');
 
 get_template_part('parts/home', 'society');
@@ -41,23 +31,15 @@ printf('
 ');
 /* Fin de fila Ciencia y Sociedad
 -------------------------------------------------------------------------------------------------------*/
-
 // Sección Laboratorios
 get_template_part('parts/home', 'laboratorios');
-
 // Sección Cultura y Agenda Cultural
 get_template_part('parts/home', 'culture');
-
 // Sección Pruebas
 get_template_part('parts/home', 'fotogalerias');
-
 // Agenda Universitaria
 get_template_part('parts/home', 'university-agenda');
-
 // Sección UNSL TV
 //get_template_part('parts/home', 'unsltv');
-
 get_footer();
-
-
 ?>
