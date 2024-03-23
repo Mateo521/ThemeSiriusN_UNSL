4b8e022b2a4c0aaebb878fd5c875d4edb7f80cc7
Mateo521 <66658355+Mateo521@users.noreply.github.com>
2024-03-23 19:56:32 -0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -6,6 +6,8 @@
         <div id="404-page" <?php post_class(); ?>>
             <div class="entry-body">
     
+               
+                
                 <span>404</span>       
             </div>
         </div>
